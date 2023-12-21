dc8628f79ec7b87c93df85a3da31c8d1e11444c9
Wilson Leandro <wilson.bleandro@gmail.com>
2023-12-21 15:37:34 -0400

    refactor StatusPage add AuthenticationException

--- src/main/kotlin/br/com/task/plugins/StatusPage.kt
+++ src/main/kotlin/br/com/task/plugins/StatusPage.kt
@@ -10,7 +10,15 @@ fun Application.configureStatusPage() {
         exception<TaskNotFoundException> { call, cause ->
             call.respond(HttpStatusCode.NotFound, cause.message.toString())
         }
+        exception<Throwable> { call, cause ->
+            if (cause is AuthenticationException) {
+                call.respond(HttpStatusCode.Unauthorized)
+            } else {
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
     }
 }
 
 class TaskNotFoundException(message: String) : Throwable(message)
+class AuthenticationException(override val message: String?) : Throwable(message)
